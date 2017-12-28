71c88556b15de249a2a919239beb1954cd2aa70c
Mark Lise <mark@digitalspace.ca>
2017-12-28 15:54:48 -0800

    Structural changes to api, add document routes/controllers, fix application definitions, miscellaneous UI fixes for swagger.

---
+++ api/controllers/document.js
@@ -0,0 +1,133 @@
+var auth        = require("../helpers/auth");
+var _           = require('lodash');
+var defaultLog  = require('winston').loggers.get('default');
+var mongoose    = require('mongoose');
+
+exports.protectedOptions = function (args, res, rest) {
+  res.status(200).send();
+}
+
+exports.publicGet = function (args, res, next) {
+  // Build match query if on docId route
+  var query = {};
+  if (args.swagger.params.docId) {
+    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.docId.value)};
+  }
+
+  getDocuments(['public'], query, args.swagger.params.fields.value)
+  .then(function (data) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(data));
+  });
+};
+exports.protectedGet = function(args, res, next) {
+  var self        = this;
+  self.scopes     = args.swagger.params.auth_payload.scopes;
+
+  var Document = mongoose.model('Document');
+  var User        = mongoose.model('User');
+
+  defaultLog.info("args.swagger.params:", args.swagger.params.auth_payload.scopes);
+
+  // Build match query if on docId route
+  var query = {};
+  if (args.swagger.params.docId) {
+    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.docId.value)};
+  }
+
+  getDocuments(args.swagger.params.auth_payload.scopes, query, args.swagger.params.fields.value)
+  .then(function (data) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(data));
+  });
+};
+
+//  Create a new application
+exports.protectedPost = function (args, res, next) {
+  var obj = args.swagger.params.app.value;
+  defaultLog.info("Incoming new object:", obj);
+
+  var Document = mongoose.model('Document');
+  var app = new Document(obj);
+  app.save()
+  .then(function (a) {
+    defaultLog.info("Saved new application object:", a);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(a));
+  });
+};
+
+// Update an existing application
+exports.protectedPut = function (args, res, next) {
+  var objId = args.swagger.params.docId.value;
+  defaultLog.info("ObjectID:", args.swagger.params.docId.value);
+  var obj = args.swagger.params.app.value;
+  defaultLog.info("Incoming updated object:", obj);
+
+  var Document = require('mongoose').model('Document');
+  Document.findOneAndUpdate({_id: objId}, obj, {upsert:false, new: true}, function (err, o) {
+    if (o) {
+      defaultLog.info("o:", o);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(o));
+    } else {
+      defaultLog.info("Couldn't find that object!");
+      res.writeHead(404, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify({}));
+    }
+  });
+}
+
+var getDocuments = function (role, query, fields) {
+  return new Promise(function (resolve, reject) {
+    var Document = mongoose.model('Document');
+    var projection = {};
+
+    // Fields we always return
+    var defaultFields = ['_id',
+                        'tags'];
+    _.each(defaultFields, function (f) {
+        projection[f] = 1;
+    });
+
+    // Add requested fields - sanitize first by including only those that we can/want to return
+    var sanitizedFields = _.remove(fields, function (f) {
+      return (_.indexOf(['displayName',
+                         'documentFileName',
+                         'internalMime'], f) !== -1);
+    });
+    _.each(sanitizedFields, function (f) {
+      projection[f] = 1;
+    });
+
+    Document.aggregate([
+      {
+        "$match": query
+      },
+      {
+        "$project": projection
+      },
+      {
+        $redact: {
+         $cond: {
+            if: {
+              $anyElementTrue: {
+                    $map: {
+                      input: "$tags" ,
+                      as: "fieldTag",
+                      in: { $setIsSubset: [ "$$fieldTag", role ] }
+                    }
+                  }
+                },
+              then: "$$DESCEND",
+              else: "$$PRUNE"
+            }
+          }
+        }
+    ]).exec()
+    .then(function (data) {
+      defaultLog.info("data:", data);
+      resolve(data);
+    });
+  });
+};
--- api/helpers/models/document.js
+++ api/helpers/models/document.js
@@ -2,6 +2,8 @@
     // _addedBy         : { type:'ObjectId', ref:'User', default:null },
     // _application     : { type:'ObjectId', ref:'Application', default:null },
     documentFileName : { type:String, default:'' },
-	internalURL      : { type:String, default:'' },
-	internalMime     : { type:String, default:'' }
+    tags             : { type: String, trim: true, default: '' },
+    displayName      : { type:String, default:'' },
+    internalURL      : { type:String, default:'' },
+    internalMime     : { type:String, default:'' }
 });
