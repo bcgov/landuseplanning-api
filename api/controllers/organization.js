13f11c310e23b3b97b92dd2c94ee1f02bac58c24
Mark Lise <mark@digitalspace.ca>
2017-12-29 11:04:34 -0800

    Add API methods for organizations.  Document API, methods, and logically group them.  Added tags for all methods, cleanup.

--- api/controllers/organization.js
+++ api/controllers/organization.js
@@ -2,25 +2,19 @@ var auth        = require("../helpers/auth");
 var _           = require('lodash');
 var defaultLog  = require('winston').loggers.get('default');
 var mongoose    = require('mongoose');
-var mime        = require('mime-types');
-var FlakeIdGen  = require('flake-idgen'),
-    intformat   = require('biguint-format'),
-    generator   = new FlakeIdGen;
-var fs          = require('fs');
-var uploadDir   = process.env.UPLOAD_DIRECTORY || "./uploads/";
 
 exports.protectedOptions = function (args, res, rest) {
   res.status(200).send();
 }
 
 exports.publicGet = function (args, res, next) {
-  // Build match query if on docId route
+  // Build match query if on orgId route
   var query = {};
-  if (args.swagger.params.docId) {
-    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.docId.value)};
+  if (args.swagger.params.orgId) {
+    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.orgId.value)};
   }
 
-  getDocuments(['public'], query, args.swagger.params.fields.value)
+  getOrganizations(['public'], query, args.swagger.params.fields.value)
   .then(function (data) {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(data));
@@ -30,67 +24,53 @@ exports.protectedGet = function(args, res, next) {
   var self        = this;
   self.scopes     = args.swagger.params.auth_payload.scopes;
 
-  var Document = mongoose.model('Document');
+  var Organization = mongoose.model('Organization');
   var User        = mongoose.model('User');
 
   defaultLog.info("args.swagger.params:", args.swagger.params.auth_payload.scopes);
 
-  // Build match query if on docId route
+  // Build match query if on orgId route
   var query = {};
-  if (args.swagger.params.docId) {
-    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.docId.value)};
+  if (args.swagger.params.orgId) {
+    query = { "_id": mongoose.Types.ObjectId(args.swagger.params.orgId.value)};
   }
 
-  getDocuments(args.swagger.params.auth_payload.scopes, query, args.swagger.params.fields.value)
+  getOrganizations(args.swagger.params.auth_payload.scopes, query, args.swagger.params.fields.value)
   .then(function (data) {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(data));
   });
 };
 
-//  Create a new document
+//  Create a new organization
 exports.protectedPost = function (args, res, next) {
-  console.log("Creating new object");
-  var obj = args.swagger.params.doc.value;
+  var obj = args.swagger.params.app.value;
   defaultLog.info("Incoming new object:", obj);
 
-  var Document = mongoose.model('Document');
-  var doc = new Document(obj);
+  var Organization = mongoose.model('Organization');
+  var app = new Organization(obj);
   // Define security tag defaults
-  doc.tags = [['sysadmin']];
-  doc.save()
-  .then(function (d) {
-    defaultLog.info("Saved new document object:", d);
+  app.tags = [['sysadmin']];
+  app.save()
+  .then(function (a) {
+    defaultLog.info("Saved new organization object:", a);
     res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify(d));
+    return res.end(JSON.stringify(a));
   });
 };
 
-// Update an existing document
+// Update an existing organization
 exports.protectedPut = function (args, res, next) {
-  // defaultLog.info("upfile:", args.swagger.params.upfile);
-  var objId = args.swagger.params.docId.value;
-  defaultLog.info("ObjectID:", args.swagger.params.docId.value);
+  var objId = args.swagger.params.orgId.value;
+  defaultLog.info("ObjectID:", args.swagger.params.orgId.value);
 
-  var guid = intformat(generator.next(), 'dec');
-  var ext = mime.extension(args.swagger.params.upfile.value.mimetype);
-  try {
-    fs.writeFileSync(uploadDir+guid+"."+ext, args.swagger.params.upfile.value.buffer);
-  } catch (e) {
-    defaultLog.info("Error:", e);
-    res.writeHead(400, { "Content-Type": "application/json" });
-    // Delete the path details before we return to the caller.
-    delete e['path'];
-    return res.end(JSON.stringify(e));
-  }
-
-  var obj = args.swagger.params;
+  var obj = args.swagger.params.app.value;
   // Strip security tags - these will not be updated on this route.
   delete obj.tags;
   defaultLog.info("Incoming updated object:", obj);
 
-  var Document = require('mongoose').model('Document');
-  Document.findOneAndUpdate({_id: objId}, obj, {upsert:false, new: true}, function (err, o) {
+  var Organization = require('mongoose').model('Organization');
+  Organization.findOneAndUpdate({_id: objId}, obj, {upsert:false, new: true}, function (err, o) {
     if (o) {
       defaultLog.info("o:", o);
       res.writeHead(200, { "Content-Type": "application/json" });
@@ -103,13 +83,15 @@ exports.protectedPut = function (args, res, next) {
   });
 }
 
-var getDocuments = function (role, query, fields) {
+var getOrganizations = function (role, query, fields) {
   return new Promise(function (resolve, reject) {
-    var Document = mongoose.model('Document');
+    var Organization = mongoose.model('Organization');
     var projection = {};
 
     // Fields we always return
     var defaultFields = ['_id',
+                        'code',
+                        'name',
                         'tags'];
     _.each(defaultFields, function (f) {
         projection[f] = 1;
@@ -117,15 +99,14 @@ var getDocuments = function (role, query, fields) {
 
     // Add requested fields - sanitize first by including only those that we can/want to return
     var sanitizedFields = _.remove(fields, function (f) {
-      return (_.indexOf(['displayName',
-                         'documentFileName',
-                         'internalMime'], f) !== -1);
+      return (_.indexOf(['name',
+                        'code'], f) !== -1);
     });
     _.each(sanitizedFields, function (f) {
       projection[f] = 1;
     });
 
-    Document.aggregate([
+    Organization.aggregate([
       {
         "$match": query
       },
