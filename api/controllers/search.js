1eb78a0e949f55d4a5d05b65d9d6c35733628a41
Severin Beauvais <severin.beauvais@gmail.com>
2018-08-17 14:32:59 -0700

    PRC-575: return sidsFound on BCGW DTID search PRC-608: don't return un-published apps PRC-465: pad CLID with leading 0s to make it 7 digits PRC-610: fix early returns on findOne calls NOBUG: don't return deleted apps

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -7,26 +7,26 @@ var request     = require('request');
 
 exports.protectedOptions = function (args, res, rest) {
   res.status(200).send();
-}
+};
 
 exports.publicGetClientsInfoByDispositionId = function (args, res, next) {
   var dtId = args.swagger.params.dtId.value;
   defaultLog.info("Searching arcgis for client info on Disposition Transaction ID:", dtId);
-  
+
   var searchURL = "http://maps.gov.bc.ca/arcgis/rest/services/mpcm/bcgw/MapServer/dynamicLayer/query?layer=%7B%22id%22%3A1%2C%22source%22%3A%7B%22type%22%3A%22dataLayer%22%2C%22dataSource%22%3A%7B%22type%22%3A%22table%22%2C%22workspaceId%22%3A%22MPCM_ALL_PUB%22%2C%22dataSourceName%22%3A%22WHSE_TANTALIS.TA_INTEREST_HOLDER_VW%22%7D%7D%7D&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&returnDistinctValues=false&f=json&where=DISPOSITION_TRANSACTION_SID=";
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + dtId + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + dtId + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('ArcGIS Call Complete.', body);
+          defaultLog.info('ArcGIS Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
         }
         var clients = [];
         _.each(obj.features, function (i) {
@@ -38,64 +38,70 @@ exports.publicGetClientsInfoByDispositionId = function (args, res, next) {
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
 
+// Get BCGW features by CLID
 exports.publicGetBCGW = function (args, res, next) {
   // Build match query if on appId route
-  var clFile = args.swagger.params.crownLandsId.value;
-  defaultLog.info("Searching BCGW for CLFILE:", clFile);
+  // Pad with leading zeros to make CLID seven digits
+  var clid = _.padStart(args.swagger.params.crownLandsId.value, 7, '0');
+  defaultLog.info("Searching BCGW for CLID:", clid);
 
   // TODO: Error handling.
 
   // var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=pub:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=application/json&PROPERTYNAME=CROWN_LANDS_FILE&CQL_FILTER=CROWN_LANDS_FILE=";
   var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=PUB:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=json&srsName=EPSG:4326&CQL_FILTER=CROWN_LANDS_FILE=";
-  console.log("SEARCHING:", searchURL+ "'" + clFile + "'");
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + clFile + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + clid + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('BCGW Call Complete.', body);
+          defaultLog.info('BCGW Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
           resolve(obj);
         }
 
-        // Search for this in our DB in case it's been imported in an application.
+        // Search for this in our DB in case it's already been imported.
         try {
           var result = _.chain(obj.features)
-          .groupBy("properties.DISPOSITION_TRANSACTION_SID")
-          .toPairs()
-          .map(function(currentItem) {
+            .groupBy("properties.DISPOSITION_TRANSACTION_SID")
+            .toPairs()
+            .map(function (currentItem) {
               return _.zipObject(["SID", "sids"], currentItem);
-          })
-          .value();
+            })
+            .value();
 
           obj.sidsFound = [];
-          result.reduce (function (current, code) {
-            return current.then (function () {
-                // console.log ('+++++++++', code.SID);
-                var Application = require('mongoose').model('Application');
-                return Application.findOne({tantalisID: code.SID}, function (err, o) {
-                if (o) {
-                  obj.sidsFound.push(code.SID);
-                } else {
-                  console.log("Nothing found");
-                }
+          result.reduce(function (current, code) {
+            return current.then(function () {
+              var Application = require('mongoose').model('Application');
+              return new Promise(function (complete, fail) {
+                Application.findOne({ tantalisID: code.SID, isDeleted: false }, function (err, o) {
+                  if (err) {
+                    fail();
+                  }
+                  if (o) {
+                    obj.sidsFound.push(code.SID); // NB: SID is string (tantalisID is number)
+                  } else {
+                    console.log("Nothing found");
+                  }
+                  complete();
+                });
               });
             });
           }, Promise.resolve())
-          .then(function () {
-            resolve(obj);
-          });
+            .then(function () {
+              resolve(obj);
+            });
         } catch (e) {
           // Error, don't tag the isImported on it.
           resolve(obj);
@@ -105,7 +111,7 @@ exports.publicGetBCGW = function (args, res, next) {
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
@@ -113,60 +119,97 @@ exports.publicGetBCGW = function (args, res, next) {
 // Get local shapes
 exports.publicGetDispositionTransactionId = function (args, res, next) {
   return new Promise(function (resolve, reject) {
-      var Feature     = require('mongoose').model('Feature');
+    var Feature = require('mongoose').model('Feature');
 
-      var query = {};
-      if (args.swagger.params.dtId && args.swagger.params.dtId.value !== undefined) {
-        _.assignIn(query, { 'properties.DISPOSITION_TRANSACTION_SID': args.swagger.params.dtId.value });
+    var query = {};
+    if (args.swagger.params.dtId && args.swagger.params.dtId.value !== undefined) {
+      _.assignIn(query, { 'properties.DISPOSITION_TRANSACTION_SID': args.swagger.params.dtId.value });
+    }
+
+    return Feature.find(query, function (err, data) {
+      if (err) {
+        return Actions.sendResponse(res, 400, err);
+      } else {
+        var featureCollection = {};
+        featureCollection.crs = {};
+        featureCollection.crs.properties = {};
+        featureCollection.crs.properties.name = "urn:ogc:def:crs:EPSG::4326";
+        featureCollection.totalFeatures = data.length;
+        featureCollection.features = data;
+        featureCollection.type = "FeatureCollection";
+        return Actions.sendResponse(res, 200, featureCollection);
       }
-
-      return Feature.find(query, function (err, data) {
-        if (err) {
-          return Actions.sendResponse(res, 400, err);
-        } else {
-          var featureCollection = {};
-          featureCollection.crs = {};
-          featureCollection.crs.properties = {};
-          featureCollection.crs.properties.name = "urn:ogc:def:crs:EPSG::4326";
-          featureCollection.totalFeatures = data.length;
-          featureCollection.features = data;
-          featureCollection.type = "FeatureCollection";
-          return Actions.sendResponse(res, 200, featureCollection);
-        }
     });
   });
 };
 
-// Get bcgw shapes
+// Get BCGW features by DTID
 exports.publicGetBCGWDispositionTransactionId = function (args, res, next) {
-  // Build match query if on appId route
+  // Build match query if on dtId route
   var dtId = args.swagger.params.dtId.value;
-  defaultLog.info("Searching BCGW for Disposition Transaction ID:", dtId);
+  defaultLog.info("Searching BCGW for DTID:", dtId);
 
   // TODO: Error handling.
 
   var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=PUB:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=json&srsName=EPSG:4326&CQL_FILTER=DISPOSITION_TRANSACTION_SID=";
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + dtId + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + dtId + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('BCGW Call Complete.', body);
+          defaultLog.info('BCGW Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
+          resolve(obj);
         }
-        resolve(obj);
+
+        // Search for this in our DB in case it's already been imported.
+        try {
+          var result = _.chain(obj.features)
+            .groupBy("properties.DISPOSITION_TRANSACTION_SID")
+            .toPairs()
+            .map(function (currentItem) {
+              return _.zipObject(["SID", "sids"], currentItem);
+            })
+            .value();
+
+          obj.sidsFound = [];
+          result.reduce(function (current, code) {
+            return current.then(function () {
+              var Application = require('mongoose').model('Application');
+              return new Promise(function (complete, fail) {
+                Application.findOne({ tantalisID: code.SID, isDeleted: false }, function (err, o) {
+                  if (err) {
+                    fail();
+                  }
+                  if (o) {
+                    obj.sidsFound.push(code.SID); // NB: SID is string (tantalisID is number)
+                  } else {
+                    console.log("Nothing found");
+                  }
+                  complete();
+                });
+              });
+            });
+          }, Promise.resolve())
+            .then(function () {
+              resolve(obj);
+            });
+        } catch (e) {
+          // Error, don't tag the isImported on it.
+          resolve(obj);
+        }
       }
     });
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
