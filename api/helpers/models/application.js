5a32e88c67ab6d81a34de2240359ad5739b39257
Severin Beauvais <severin.beauvais@gmail.com>
2018-10-03 11:05:12 -0700

    PRC-718: added Legal Description to update + calculated areaHectares on POST and update + cleaned up app model fields

--- api/helpers/models/application.js
+++ api/helpers/models/application.js
@@ -1,36 +1,36 @@
  module.exports = require ('../models')('Application', {
     _addedBy         : { type:'ObjectId', ref:'User', default:null },
     _proponent       : { type:'ObjectId', ref:'Organization', default:null },
-    code             : { type: String, trim: true, default: ''},
-    name             : { type: String, trim: true },
-    // Note: Default on tag property is purely for display only, they have no real effect on the model
-    // This must be done in the code.
-    tags             : [[{ type: String, trim: true, default: '[["sysadmin"]]' }]],
-    type             : { type: String },
-    subtype          : { type: String },
-    purpose          : { type: String },
-    subpurpose       : { type: String },
-    status           : { type: String },
-    tenureStage      : { type: String },
-    location         : { type: String, default: '' },
+
+    agency           : { type:String, default: '' },
+    areaHectares     : { type: Number, default: 0.00 },
+    businessUnit     : { type: String },
+
     // Centroid (in coordinates) of all features associated with this application.
     centroid         : [{ type: Number, default: 0.00}],
-    areaHectares     : { type: Number, default: 0.00 },
-    publishDate      : { type: Date, default: Date.now },
-    businessUnit     : { type: String },
-    legalDescription : { type: String },
-    agency           : { type:String, default: '' },
+
     cl_file          : { type:Number, default: 0 },
-    mapsheet         : { type:String, default: '' },
-    description      : { type:String, default: '' },
-    tantalisID       : { type:Number, default: 0 },
-    internalID       : { type:Number, default: 0 },
-    interestID       : { type:Number, default: 0 },
-    isDeleted        : { type: Boolean, default: false },
-    postID           : { type:Number, default: 0 },
     client           : { type:String, default: '' },
+    code             : { type: String, trim: true, default: ''},
+    description      : { type:String, default: '' },
     internal: {
         notes   : { type: String, default: '' },
         tags    : [[{ type: String, trim: true, default: '[["sysadmin"]]' }]]
     },
+    isDeleted        : { type: Boolean, default: false },
+    legalDescription : { type: String },
+    location         : { type: String, default: '' },
+    name             : { type: String, trim: true },
+    publishDate      : { type: Date, default: Date.now },
+    purpose          : { type: String },
+    status           : { type: String },
+    subpurpose       : { type: String },
+    subtype          : { type: String },
+    tantalisID       : { type:Number, default: 0 },
+    tenureStage      : { type: String },
+    type             : { type: String },
+
+    // Note: Default on tag property is purely for display only, they have no real effect on the model.
+    // This must be done in the code.
+    tags             : [[{ type: String, trim: true, default: '[["sysadmin"]]' }]],
 });
