4742c3f4df29f2eac597b71a1c1b46ff51d27957
Severin Beauvais <severin.beauvais@gmail.com>
2019-01-30 11:41:50 -0800

    PRC-612: cleaned up Swagger file - removed 'fields' from head META calls - removed 'fields' from publicHead function (not needed) - removed 'isDeleted' from public calls - standardized summaries and descriptions - identified all authenticated calls - added missing definitions - updated object definitions (properties + enums)

--- api/helpers/models/application.js
+++ api/helpers/models/application.js
@@ -1,17 +1,17 @@
  module.exports = require ('../models')('Application', {
-    agency           : { type:String, default: '' },
+    agency           : { type: String, default: '' },
     areaHectares     : { type: Number, default: 0.00 },
     businessUnit     : { type: String },
 
     // Centroid (in coordinates) of all features associated with this application.
-    centroid         : [{ type: Number, default: 0.00}],
+    centroid         : [{ type: Number, default: 0.00}], // updated by daily script
 
-    cl_file          : { type:Number, default: 0 },
-    client           : { type:String, default: '' },
-    _createdBy       : { type: String, default: '' },
-    createdDate      : { type: Date, default: Date.now },
-    description      : { type:String, default: '' },
-    isDeleted        : { type: Boolean, default: false },
+    cl_file          : { type: Number, default: 0 },
+    client           : { type: String, default: '' },
+    _createdBy       : { type: String, default: '' }, // updated by API
+    createdDate      : { type: Date, default: Date.now }, // updated by API
+    description      : { type: String, default: '' },
+    isDeleted        : { type: Boolean, default: false }, // updated by API
     legalDescription : { type: String },
     location         : { type: String, default: '' },
     name             : { type: String, trim: true },
@@ -20,7 +20,7 @@
     status           : { type: String },
     subpurpose       : { type: String },
     subtype          : { type: String },
-    tantalisID       : { type:Number, default: 0 },
+    tantalisID       : { type: Number, default: 0 },
     tenureStage      : { type: String },
     type             : { type: String },
 
@@ -29,5 +29,5 @@
 
     // Note: Default on tag property is purely for display only, they have no real effect on the model.
     // This must be done in the code.
-    tags             : [[{ type: String, trim: true, default: '[["sysadmin"]]' }]],
+    tags             : [[{ type: String, trim: true, default: '[["sysadmin"]]' }]], // updated by API
 });
