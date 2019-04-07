a363b06f1a938fc86ce57a64384c556b9fb32da9
Mark Lisé <mark@digitalspace.ca>
2019-04-07 10:06:47 -0700

    Updating index creation

--- migrations/20190404201820-listItems.js
+++ migrations/20190404201820-listItems.js
@@ -14,6 +14,8 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
+let listItems = require(process.cwd() + '/migrations_data/lists');
+
 exports.up = function(db) {
   let mClient;
   return db.connection.connect(db.connectionString, { native_parser: true })
@@ -21,24 +23,22 @@ exports.up = function(db) {
       // mClientInst is an instance of MongoClient
       mClient = mClientInst;
       var p = mClient.collection('epic');
-      p.createIndex( {
-        displayName: "text",
-        name: "text",
-        description: "text",
-        eacDecision: "text",
-        location: "text",
-        region: "text",
-        commodity: "text",
-        type: "text",
-        epicProjectId: "text",
-        sector: "text",
-        status: "text",
-        code: "text" },
-        {
-          name: "searchIndex"
+      p.insertMany(
+        listItems
+        )
+        .then(function (arr) {
+          console.log("arr:", arr)
+        for(let item of arr.ops) {
+          p.update(
+          {
+            _id: item._id
+          },
+          {
+            $set: { read: ['public','project-system-admin', 'sysadmin'], write: ['project-system-admin', 'sysadmin'] }
+          });
         }
-      );
-      mClient.close();
+        mClient.close();
+      });
     })
     .catch((e) => {
       console.log("e:", e);
