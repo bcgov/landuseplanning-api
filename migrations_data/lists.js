ec68ab1190180e84865c3577b94a43e5ea8f2519
Mark Lisé <mark@digitalspace.ca>
2019-04-10 13:59:04 -0700

    List data seed

---
+++ migrations_data/lists.js
@@ -0,0 +1,186 @@
+module.exports = [{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Assessment"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Comments/Submissions"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Correspondence"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Report"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Letter"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Notice/News Release"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Notification"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Order"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Notice/News Release"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Notification"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Order"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Project Information"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Report/Plan"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Request"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Presentation"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Amendment"
+},{
+  "type" : "doctype",
+  "_schemaName" : "List",
+  "name" : "Fees"
+},
+////////////////////////////////////////////////
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Proponent"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Certificate Holder"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Minister's Office"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "EAO"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Participating Indigenous Nation"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Public"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Other Agency"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "CEAA"
+},
+{
+  "type" : "author",
+  "_schemaName" : "List",
+  "name" : "Working Group"
+},
+////////////////////////////////////////////////
+{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Transition Order"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Minister's Designation (S6)"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Opt-in (S7)"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "S.10(1)(a) Determination"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "S10(1)(b) Determination"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "S10(1)(c) Determination"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "S.11 Order"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Initial Project Description"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Project Description"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Draft Application Information Requirements"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Application information Requirements"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Draft Application"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Application"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Referral Package"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Certificate Package"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Substantially Started Determination"
+},{
+  "type" : "label",
+  "_schemaName" : "List",
+  "name" : "Certificate Extension Determination"
+}
+]
