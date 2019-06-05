d4c42ac4cefb615c18630de504165fec0f5b6027
Mark Lisé <mark@digitalspace.ca>
2019-06-05 10:14:32 -0700

    Tweaks to cache

--- app.js
+++ app.js
@@ -45,9 +45,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,responseType');
   res.setHeader('Access-Control-Expose-Headers', 'x-total-count');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  // res.setHeader('Cache-Control', 'max-age=600');
-  res.setHeader('Expires', '-1');
-  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Cache-Control', 'max-age=60');
   next();
 });
 
