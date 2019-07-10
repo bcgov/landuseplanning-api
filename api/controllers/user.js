d37739b1fff80681ede8f0eec98058eb64bc1178
Daniel Truong <daniel@dtsoftware.ca>
2019-07-10 09:42:35 -0700

    Removing get user (#96)

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,35 +1,12 @@
-var auth        = require("../helpers/auth");
-var _           = require('lodash');
-var defaultLog  = require('winston').loggers.get('default');
-var mongoose    = require('mongoose');
-var Actions     = require('../helpers/actions');
-var Utils       = require('../helpers/utils');
+var auth = require("../helpers/auth");
+var _ = require('lodash');
+var defaultLog = require('winston').loggers.get('default');
+var mongoose = require('mongoose');
+var Actions = require('../helpers/actions');
 
 exports.protectedOptions = function (args, res, rest) {
   res.status(200).send();
 }
-
-exports.protectedGet = function(args, res, next) {
-  var self        = this;
-  self.scopes     = args.swagger.params.auth_payload.realm_access.roles;
-
-  var User = mongoose.model('User');
-
-  defaultLog.info("args.swagger.params:", args.swagger.params.auth_payload.realm_access.roles);
-
-  // Build match query if on userId route
-  var query = {};
-  if (args.swagger.params.userId) {
-    query = Utils.buildQuery("_id", args.swagger.params.userId.value, query);
-  }
-
-  console.log(args.swagger.params.fields.value);
-
-  getUsers(self.scopes, query, args.swagger.params.fields.value)
-  .then(function (data) {
-    return Actions.sendResponse(res, 200, data);
-  });
-};
 
 //  Create a new user
 exports.protectedPost = function (args, res, next) {
@@ -46,10 +23,10 @@ exports.protectedPost = function (args, res, next) {
   // Define security tag defaults - users not public by default.
   user.tags = [['sysadmin']];
   user.save()
-  .then(function (a) {
-    defaultLog.info("Saved new user object:", a);
-    return Actions.sendResponse(res, 200, a);
-  });
+    .then(function (a) {
+      defaultLog.info("Saved new user object:", a);
+      return Actions.sendResponse(res, 200, a);
+    });
 };
 
 // Update an existing user
@@ -57,7 +34,7 @@ exports.protectedPut = function (args, res, next) {
   var objId = args.swagger.params.userId.value;
   defaultLog.info("ObjectID:", args.swagger.params.userId.value);
 
-  this.scopes     = args.swagger.operation["x-security-scopes"];
+  this.scopes = args.swagger.operation["x-security-scopes"];
 
   var obj = args.swagger.params.user.value;
   // NB: Don't strip security tags on protectedPut.  Only sysadmins
@@ -89,11 +66,11 @@ exports.protectedPut = function (args, res, next) {
 
   if (obj.username && obj.username === 'admin' && !_.some(obj.roles, _.method('includes', 'sysadmin'))) {
     // No way, can't change admin user.
-    return Actions.sendResponse(res, 405, {message: '405: Admin user must have sysadmin role.'});
+    return Actions.sendResponse(res, 405, { message: '405: Admin user must have sysadmin role.' });
   }
 
   var User = require('mongoose').model('User');
-  User.findOneAndUpdate({_id: objId}, obj, {upsert:false, new: true}, function (err, o) {
+  User.findOneAndUpdate({ _id: objId }, obj, { upsert: false, new: true }, function (err, o) {
     if (o) {
       defaultLog.info("o:", o);
       return Actions.sendResponse(res, 200, o);
@@ -103,52 +80,3 @@ exports.protectedPut = function (args, res, next) {
     }
   });
 }
-
-var getUsers = function (role, query, fields) {
-  return new Promise(function (resolve, reject) {
-    var User = mongoose.model('User');
-    var projection = {};
-
-    // Fields we always return
-    var defaultFields = ['_id',
-                        'displayName',
-                        'roles',
-                        'tags'];
-    _.each(defaultFields, function (f) {
-        projection[f] = 1;
-    });
-
-    // Add requested fields - sanitize first by including only those that we can/want to return
-    var sanitizedFields = _.remove(fields, function (f) {
-      return (_.indexOf(['displayName', 'firstName', 'lastName', 'username', 'roles', 'org', 'email', 'phoneNumber'], f) !== -1);
-    });
-    _.each(sanitizedFields, function (f) {
-      projection[f] = 1;
-    });
-
-    // No need to redact because 'sysadmin'
-    User.aggregate([
-      {
-        "$match": query
-      },
-      {
-        "$project": projection
-      },
-      {
-        "$lookup": {
-          "from": "epic",
-          "localField": "org",
-          "foreignField": "_id",
-          "as": "org"
-        }
-      },
-      {
-        "$unwind": "$org"
-      }
-    ]).exec()
-    .then(function (data) {
-      defaultLog.info("data:", data);
-      resolve(data);
-    });
-  });
-};
