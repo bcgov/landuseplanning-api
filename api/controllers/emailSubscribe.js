f43f470fd23de68d51250a4c34a8d040b00392a0
SteveMHoward <Steve.M.Howard@gov.bc.ca>
2020-04-21 11:34:38 -0700

    Continued updates to the email subscribe feature

--- api/controllers/emailSubscribe.js
+++ api/controllers/emailSubscribe.js
@@ -99,7 +99,7 @@ exports.unProtectedPost = async function (args, res, next) {
     // New project for an existing email
     existingProjectArray.push(mongoose.Types.ObjectId(obj.project));
     var es = await EmailSubscribe.update({ _id: existingEmailId }, { $set: { project: existingProjectArray } });
-    Utils.recordAction('Put', 'EmailSubscribe', 'public', emailSubscribe.email);
+    Utils.recordAction('Put', 'EmailSubscribe', 'public', existingEmailId);
     defaultLog.info('New project added to email subscribe:', es);
     return Actions.sendResponse(res, 200, es);
   }
@@ -178,7 +178,7 @@ exports.unProtectedPut = async function (args, res, next) {
   }
 }
 
-// unsubscribe from ubdates
+// unsubscribe from updates
 exports.unProtectedDelete = async function (args, res, next) {
   console.log('ES Delete', args.swagger.params);
 
@@ -257,19 +257,6 @@ exports.protectedGet = async function (args, res, next) {
 
   var query = {}, sort = {}, skip = null, limit = null, count = false, filter = [];
 
-  // Build match query if on email route.
-  /*
-  if (args.swagger.params.email && args.swagger.params.email.value) {
-    _.assignIn(query, { _id: mongoose.Types.ObjectId(args.swagger.params.email.value) });
-  }*/
-
-  // Build match query if on comment period's id
-  // *** Update to project match
-  /*
-  if (args.swagger.params.period && args.swagger.params.period.value) {
-    _.assignIn(query, { period: mongoose.Types.ObjectId(args.swagger.params.period.value) });
-  }*/
-
   // Build match query for project ID
   if (args.swagger.params.project && args.swagger.params.project.value) {
     console.log('ES Project ID', args.swagger.params.project.value);
@@ -295,15 +282,9 @@ exports.protectedGet = async function (args, res, next) {
     count = args.swagger.params.count.value;
   }
 
-  //query = addStandardQueryFilters(query, args);
-
   // Set query type
   _.assignIn(query, { '_schemaName': 'EmailSubscribe' });
 
-  // Set filter for eaoStatus
-  /*if (args.swagger.params.pending && args.swagger.params.pending.value === true) {
-    filter.push({ 'eaoStatus': 'Pending' });
-  }*/
   if (filter.length !== 0) {
     _.assignIn(query, { $or: filter });
   }
@@ -320,32 +301,46 @@ exports.protectedGet = async function (args, res, next) {
       count); // count
     //Utils.recordAction('Get', 'EmailSubscribe', args.swagger.params.auth_payload.preferred_username, args.swagger.params.email && args.swagger.params.email.value ? args.swagger.params.email.value : null);
     defaultLog.info('Got email subscribers:', data);
-
-    // This is to get the next pending comment information.
-    /*
-    if (args.swagger.params.populateNextComment && args.swagger.params.populateNextComment.value) {
-      defaultLog.info('Getting next pending comment information');
-      var queryForNextComment = {};
-
-      _.assignIn(queryForNextComment, { _id: { $ne: data[0]._id } });
-      _.assignIn(queryForNextComment, { period: data[0].period });
-      _.assignIn(queryForNextComment, { eaoStatus: 'Pending' });
-
-      var nextComment = await Utils.runDataQuery('Comment',
-        args.swagger.params.auth_payload.realm_access.roles,
-        queryForNextComment,
-        [], // Fields
-        null,
-        { commentId: 1 }, // sort
-        0, // skip
-        1, // limit
-        true); // count
-      res.setHeader('x-pending-comment-count', nextComment && nextComment.length > 0 ? nextComment[0].total_items : 0);
-      res.setHeader('x-next-comment-id', nextComment && nextComment.length > 0 && nextComment[0].results.length > 0 ? nextComment[0].results[0]._id : null);
-    }*/
     return Actions.sendResponse(res, 200, data);
   } catch (e) {
     defaultLog.info('Error:', e);
     return Actions.sendResponse(res, 400, e);
   }
 };
+
+// Admin delete email
+exports.protectedDelete = async function (args, res, next) {
+  console.log('ES Delete', args.swagger.params);
+
+  // verify that the email and key have been set in the request
+  if (!(args.swagger.params.email && args.swagger.params.email.value)) {
+    return Actions.sendResponse(res, 404, 'Not found');
+  }
+
+  var emailAddress = args.swagger.params.email.value;
+  var emailId;
+  defaultLog.info('Delete email subscribe:', emailAddress);
+
+  var EmailSubscribe = mongoose.model('EmailSubscribe');
+
+  // find the object ID based on the email address
+  await EmailSubscribe.findOne({ email: emailAddress }, null, function (err, entity) {
+    try {
+      emailId = entity._id;
+    } catch (e) {
+      defaultLog.info('Error:', e);
+      return Actions.sendResponse(res, 404, e);
+    }
+  });
+
+  try {
+
+    var es = await EmailSubscribe.findOneAndRemove({ _id: emailId });
+    Utils.recordAction('Delete', 'EmailSubscribe', 'public', emailAddress);
+    defaultLog.info('Email deleted:', es);
+    return Actions.sendResponse(res, 200, es);
+  } catch (e) {
+    defaultLog.info('Error:', e);
+    return Actions.sendResponse(res, 400, e);
+  }
+}
