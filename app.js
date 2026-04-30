c4edafab6b64b1c221a854175967a7c34880f3e6
Jareth Whitney <jareth.whitney@gov.bc.ca>
2026-04-29 18:39:54 -0700

    bugfix/deseng959: Further revision to db authentication in app.js.

--- app.js
+++ app.js
@@ -93,12 +93,13 @@ swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
   const options = {
     user: dbUsername,
     pass: dbPassword,
-    authSource: 'admin',
+    authSource: process.env.MONGODB_AUTH_SOURCE || process.env.MONGODB_DATABASE,
     maxPoolSize: 10, // Maintain up to 10 socket connections
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   };
   defaultLog.info('Connecting to:', dbConnection);
+  defaultLog.info(options);
   mongoose.Promise = global.Promise;
   (mongoose.connect(dbConnection, options).then(async () => {
     defaultLog.info('Database connected');
