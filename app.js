764fd3f82a238e45d7dc3c47bbf4859810cf747a
Jareth Whitney <jareth.whitney@gov.bc.ca>
2026-04-30 11:43:13 -0700

    bugfix/deseng959: Removing index syncing after it failed in dev environment due to existing duplicates on unique index.

--- app.js
+++ app.js
@@ -126,7 +126,6 @@ swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
     require('./api/helpers/models/commentperiod');
     require('./api/helpers/models/topic');
     require('./api/helpers/models/emailSubscribe');
-    await mongoose.connection.syncIndexes();
     defaultLog.info('db model loading done.');
 
     // Build text index.
